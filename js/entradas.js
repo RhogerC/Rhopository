9551a4f07ce78d15c25709c3d8cd1ebfb2643463
RCardona <rcmbuy839@gmail.com>
2025-05-19 22:24:06 +0200

    entrdas

--- js/entradas.js
+++ js/entradas.js
@@ -1,8 +1,11 @@
 // script.js para entradas del sitio
 const entradas = [
+    { titulo: "Tema 6 Lenguaje de Marcas", "url": "https://rhogerc.github.io/Rhopository/asignaturas/LM/3EV/Tema6/Tema6LM.html", "fecha": "19-05-2025" }, 
+    { titulo: "Tema 5 Lenguaje de Marcas", "url": "https://rhogerc.github.io/Rhopository/asignaturas/LM/3EV/Tema5/Tema5LM.html", "fecha": "19-05-2025" },   
+    { titulo: "Tema 4 Lenguaje de Marcas", "url": "https://rhogerc.github.io/Rhopository/asignaturas/LM/2EV/Tema4/Tema4LM.html", "fecha": "19-05-2025" },
+    { titulo: "Tema 3 Lenguaje de Marcas", "url": "https://rhogerc.github.io/Rhopository/asignaturas/LM/1EV/Tema3/Tema3LM.html", "fecha": "05-12-2024" },
     { titulo: "Tema 2 Lenguaje de Marcas", "url": "https://rhogerc.github.io/Rhopository/asignaturas/LM/1EV/Tema2/Tema2LM.html", "fecha": "05-12-2024" },
-    { titulo: "Tema 3 Lenguaje de Marcas", url: "https://rhogerc.github.io/Rhopository/asignaturas/LM/1EV/Tema3/Tema3LM.html", fecha: "05-12-2024" },
-    { titulo: "Acerca de mi", url: "https://rhogerc.github.io/Rhopository/about.html", fecha: "05-12-2024" }
+    { titulo: "Acerca de mi", "url": "https://rhogerc.github.io/Rhopository/about.html", "fecha": "05-12-2024" }
 ];
 
 // Ordenar por fecha (últimos primero)
@@ -11,7 +14,7 @@ entradas.sort((a, b) => new Date(b.fecha) - new Date(a.fecha));
 // Renderizar en el bloque central
 const centralBlock = document.getElementById("central-block");
 entradas.forEach((entrada, index) => {
-    if (index < 5) { // Mostrar solo las 5 últimas
+    if (index < 6) { // Mostrar solo las 6 últimas
         const article = document.createElement("article");
         article.innerHTML = `
             <h3><a href="${entrada.url}" target="_self">${entrada.titulo}</a></h3>
